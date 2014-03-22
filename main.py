d6c44cbab4e1cea3ea326638700d8d91541c3a96
Marco Nilsson <marco@ormgas.com>
2014-03-22 17:05:37 +0100

    First draft implementation.

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
-import sys
+import exhub
+import config
 from api import api
+
+import logging
+import platform
+import os
+import mmap
+import serial
+
+
+def main():
+    # Setup logging
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
+
+    local_hub = exhub.Exhub('/dev/USART1')
+    configuration = config.Config('config.bin')
+
+    # iRacing memory mapped files only works on Windows systems. In case of a non-Windows system we can simulate
+    # the behavior by mapping against a memory dump instead.
+    mmap_item = None
+    if platform.system() != 'Windows':
+        mmap_file = os.path.join('api', 'tests', 'memorydump.dmp')
+        mmap_f = open(mmap_file, 'r+b')
+        mmap_item = mmap.mmap(mmap_f.fileno(), api.MEMMAPFILESIZE)
+    client_api = api.API(mmap_item)
+
+    try:
+        while True:
+            try:
+                logging.debug('Wait for request')
+                # Wait for a request from the Exhub
+                address = local_hub.blocking_read()
+                # Fetch iRacing value
+                iracing_key = config.get_definition(address).sensor_name
+                iracing_value = client_api[iracing_key]
+                # Apply reverse calculation
+                ad_reading = int(configuration.apply_formulas(address, iracing_value))
+                # Reply to Exhub
+                logging.debug('Sending sensor {} value {}'.format(iracing_key, ad_reading))
+                local_hub.send(address, ad_reading)
+            except serial.SerialException, e:
+                logging.error('Unrecoverable serial exception, {}'.format(e))
+                raise
+            except Exception, e:
+                logging.error('Unknown error, {}'.format(e))
+
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
