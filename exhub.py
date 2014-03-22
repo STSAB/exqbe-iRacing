a3cd8a25f7ba7205f24b73aed2b9feca8fe912f4
Marco Nilsson <marco@ormgas.com>
2014-03-22 17:05:20 +0100

    Add r/w functionality to exhub handler.

---
+++ exhub.py
@@ -0,0 +1,18 @@
+import serial
+
+
+class Exhub:
+
+    def __init__(self, port):
+        self.serial = serial.Serial(port, baudrate=38400)
+
+    def send(self, address, value):
+        self.serial.write('{} {}\n'.format(address, value))
+
+    def blocking_read(self):
+        """
+        Read until a line has been received.
+        If the remote Exhub follows protocol it will send the address of the sensor it wants to read, followed
+        by a newline. '123\n' cast to int will become just 123.
+        """
+        return int(self.serial.readline())
