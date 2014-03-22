e477c40a6f00f2f2ec12afffda83138d49d89bbb
Marco Nilsson <marco@ormgas.com>
2014-03-22 20:54:30 +0100

    Use configuration an not config when fetching keys. Also, be more verbose when logging errors.

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import platform
 import os
 import mmap
 import serial
-
+import sys
 
 def main():
     # Setup logging
     logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
-    local_hub = exhub.Exhub('/dev/USART1')
+    local_hub = exhub.Exhub('/dev/cu.usbserial-A603B312')
     configuration = config.Config('config.bin')
 
     # iRacing memory mapped files only works on Windows systems. In case of a non-Windows system we can simulate
@@ -32,7 +32,7 @@ def main():
                 # Wait for a request from the Exhub
                 address = local_hub.blocking_read()
                 # Fetch iRacing value
-                iracing_key = config.get_definition(address).sensor_name
+                iracing_key = configuration.get_definition(address).sensor_name
                 iracing_value = client_api[iracing_key]
                 # Apply reverse calculation
                 ad_reading = int(configuration.apply_formulas(address, iracing_value))
@@ -42,8 +42,10 @@ def main():
             except serial.SerialException, e:
                 logging.error('Unrecoverable serial exception, {}'.format(e))
                 raise
+            except KeyError, e:
+                logging.error('Unknown sensor name: {}'.format(e.message))
             except Exception, e:
-                logging.error('Unknown error, {}'.format(e))
+                logging.error('Unknown error, {}'.format(sys.exc_info()[0]))
 
     except KeyboardInterrupt:
         pass
