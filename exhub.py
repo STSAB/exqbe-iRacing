a7cb11c4885c873caf76c37bc87c243051578cc5
Marco Nilsson <marco@ormgas.com>
2014-03-27 10:13:38 +0100

    Exhub currently sends the value in decimal, not ASCII, when requesting values.

--- exhub.py
+++ exhub.py
@@ -13,6 +13,8 @@ class Exhub:
         """
         Read until a line has been received.
         If the remote Exhub follows protocol it will send the address of the sensor it wants to read, followed
-        by a newline. '123\n' cast to int will become just 123.
+        by a newline. '123\n' cast to int will become just 123. The address is currently in decimal form, not in ASCII
+        form. If the exhub wants to read a sensor with an id 10 it will send '\0x0A\n', not '10\n\. This will change.
         """
-        return int(self.serial.readline())
+        res = self.serial.readline()
+        return ord(res[0])
